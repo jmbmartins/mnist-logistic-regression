b2e7b608293ea64cbeaf6968d8e9755f9d3a9dc4
João Martins <jmbaltazarmartins@gmail.com>
2023-10-24 12:12:55 +0100

    .

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,6 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
 
 
 #########################################################################
@@ -26,9 +28,17 @@ def load_and_process_data(file_name):
     y = np.asarray(y)
 
     # Normalize the pixel values using Min-Max scaling
+    '''
     scaler = MinMaxScaler()
     scaler.fit(X)
     X = scaler.transform(X)
+    '''
+
+
+    # Normalize the pixel values using Z-score (standardization)
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
 
     # Add a bias term (1) to the input data
     X = np.append(X, np.ones((X.shape[0], 1), np.float64), axis=1)
@@ -51,12 +61,14 @@ X_test, y_test = load_and_process_data('mnist_test.csv')
 #   Logistic regression
 #########################################################################
 
-def compute_loss(y_true, y_pred):
-    # Compute the binary cross-entropy loss
+def compute_loss(y_true, y_pred, weights, lambda_reg):
+    # Compute the binary cross-entropy loss with L2 regularization
     epsilon = 1e-9
     y1 = y_true * np.log(y_pred + epsilon)
     y2 = (1 - y_true) * np.log(1 - y_pred + epsilon)
-    return -np.mean(y1 + y2)
+    regularization_term = (lambda_reg / (2 * len(y_true))) * np.sum(weights ** 2)
+    return -np.mean(y1 + y2) + regularization_term
+
 
 
 def sigmoid(x):
@@ -71,7 +83,7 @@ def feed_forward(X, weights, bias):
     return A
 
 
-def fit(X, y, n_iters, lr):
+def fit(X, y, lr, lambda_reg, stopping_threshold):
     n_samples, n_features = X.shape
     n_classes = y.shape[1]
 
@@ -79,26 +91,40 @@ def fit(X, y, n_iters, lr):
     bias = np.zeros(n_classes)
     losses = []  # To store loss at each iteration
 
-    for iteration in range(n_iters):
+    previous_loss = float('inf')  # Set to a large value initially
+    iteration = 0
+
+    while True:
+        iteration += 1
         A = feed_forward(X, weights, bias)
-        loss = compute_loss(y, A)
+        loss = compute_loss(y, A, weights, lambda_reg)
         losses.append(loss)
+
+        if abs(loss - previous_loss) < stopping_threshold:
+            print("Stopping training as loss change is smaller than the stopping threshold.")
+            break
+
         dz = A - y
-        dw = (1 / n_samples) * np.dot(X.T, dz)
+        dw = (1 / n_samples) * (np.dot(X.T, dz) + lambda_reg * weights)
         db = (1 / n_samples) * np.sum(dz, axis=0)
         weights -= lr * dw
         bias -= lr * db
 
-        if (iteration + 1) % 10 == 0:
-            print(f"Iteration {iteration + 1}/{n_iters} - Loss: {loss:.4f}")
+        previous_loss = loss
 
-    return weights, bias, losses
+        if iteration % 10 == 0:
+            print(f"Iteration {iteration} - Loss: {loss}")
 
+    return weights, bias, losses, iteration
 
+# Call the fit function with the stopping_threshold
 learning_rate = 0.001
-n_iters = 500
+lambda_reg = 0.01
+stopping_threshold = 1e-6
 
-weights, bias, losses = fit(X_train, y_train, n_iters, learning_rate)
+weights, bias, losses,n_iters = fit(X_train, y_train, learning_rate, lambda_reg, stopping_threshold)
+
+
 
 # Plot the loss over iterations
 plt.figure(1)
@@ -118,3 +144,5 @@ cm = confusion_matrix(np.argmax(y_test, axis=1), predicted_classes)
 print("Test accuracy: {0:.3f}".format(np.sum(np.diag(cm)) / np.sum(cm)))
 print("Confusion Matrix:")
 print(np.array(cm))
+
+input('Close app?')
